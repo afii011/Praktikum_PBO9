1e1f3de6b4d85d41fbf839fec17296c169d9bceb
Afifatul Ukhrowiyah <afifaukhrowiyah@gmail.com>
2024-10-22 12:02:11 +0700

    Tugas PBO 9

---
+++ Tugas9/Kucing.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas9;
+class Kucing extends Hewan {
+    @Override
+    void suara() {
+        System.out.println("Kucing mengeluarkan suara : Meow meow");
+    }
+}
---
+++ Tugas9/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas9;
+public class Main {
+    public static void main(String[] args) {
+        Hewan kucing = new Kucing();
+        Hewan anjing = new Anjing();
+        
+        kucing.suara();
+        anjing.suara();
+    }
+}
